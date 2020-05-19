cd8a06960d949fb8e93d1e52771584450ac7ce5f
gyaneshsharma <50901190+gyaneshsharma@users.noreply.github.com>
2020-05-19 12:23:30 +0530

    Create Testing.py

---
+++ Testing.py
@@ -0,0 +1,56 @@
+#Loading our classifer
+
+from keras.models import load_model
+
+classifier = load_model('Image_samples_mobileNet.h5')
+
+
+#Testing our classifer on some test images
+import os
+import cv2
+import numpy as np
+from os import listdir
+from os.path import isfile, join
+
+Image_samples_dict = {"[0]": "Ayush", 
+                      "[1]": "Shivam"}
+
+Image_samples_dict_n = {"n0": "Ayush", 
+                        "n1": "Shivam"}
+                        
+def draw_test(name, pred, im):
+    monkey = Image_samples_dict[str(pred)]
+    BLACK = [0,0,0]
+    expanded_image = cv2.copyMakeBorder(im, 80, 0, 0, 100 ,cv2.BORDER_CONSTANT,value=BLACK)
+    cv2.putText(expanded_image, monkey, (20, 60) , cv2.FONT_HERSHEY_SIMPLEX,1, (0,0,255), 2)
+    cv2.imshow(name, expanded_image)
+
+def getRandomImage(path):
+    """function loads a random images from a random folder in our test path """
+    folders = list(filter(lambda x: os.path.isdir(os.path.join(path, x)), os.listdir(path)))
+    random_directory = np.random.randint(0,len(folders))
+    path_class = folders[random_directory]
+    print("Class - " + Image_samples_dict_n[str(path_class)])
+    file_path = path + path_class
+    file_names = [f for f in listdir(file_path) if isfile(join(file_path, f))]
+    random_file_index = np.random.randint(0,len(file_names))
+    image_name = file_names[random_file_index]
+    return cv2.imread(file_path+"/"+image_name)    
+
+for i in range(0,10):
+    input_im = getRandomImage("Image_samples/Image_samples/validation/")
+    input_original = input_im.copy()
+    input_original = cv2.resize(input_original, None, fx=0.5, fy=0.5, interpolation = cv2.INTER_LINEAR)
+    
+    input_im = cv2.resize(input_im, (224, 224), interpolation = cv2.INTER_LINEAR)
+    input_im = input_im / 255.
+    input_im = input_im.reshape(1,224,224,3) 
+    
+    # Get Prediction
+    res = np.argmax(classifier.predict(input_im, 1, verbose = 0), axis=1)
+    
+    # Show image with predicted class
+    draw_test("Prediction", res, input_original) 
+    cv2.waitKey(0)
+
+cv2.destroyAllWindows()
